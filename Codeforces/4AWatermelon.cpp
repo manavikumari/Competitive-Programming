c66ffca45480d88d4a2aea6223b0c35bcadc1c47
manavikumari <ommanavi.ku@gmail.com>
2022-08-07 00:04:09 +0530

    First codeforces problem

---
+++ Codeforces/4AWatermelon.cpp
@@ -0,0 +1,40 @@
+// In the name of Aadi Shakti
+// We are nothing and you are everything
+// Jai Mata Di
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fast ios_base::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0) ;
+#define endl '\n'
+
+void solve()
+{
+  int n ;
+  cin >> n ;
+  if (n % 2 == 0 and n > 2) {
+    cout << "YES" << endl ;
+  }
+  else {
+    cout << "NO" << endl ;
+  }
+}
+
+int32_t main()
+{
+    int testcases = 1 ;
+    //cin >> testcases ;
+    while(testcases--)
+    {
+        solve() ;
+    }
+    return 0 ;
+}
+/*  CODED BY:-
+ _______________________________
+|                              |
+| |\  /|  /\  |\ |  /\  \  / | |
+| | \/ | /~~\ | \| /~~\  \/  | |
+|______________________________|
+
+*/
